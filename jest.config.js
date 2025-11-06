7d3d24fa44a14a1086e7b364051d29af1c950844
gioblu <gioscarab@gmail.com>
2025-11-06 10:18:11 +0100

    added jest configuration

---
+++ jest.config.js
@@ -0,0 +1,28 @@
+
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  maxWorkers: 1,  // Run tests sequentially to prevent race conditions with shared database
+  testMatch: ['**/tests/**/*.test.ts', '**/tests/**/*.spec.ts'],
+  testPathIgnorePatterns: ['/node_modules/', '/dist/'],
+  setupFilesAfterEnv: ['<rootDir>/tests/jest.setup.ts'],
+  collectCoverageFrom: [
+    'src/**/*.ts',
+    '!src/**/*.d.ts',
+    '!src/index.ts',
+    '!src/openapi/**',
+  ],
+  coveragePathIgnorePatterns: [
+    '/node_modules/',
+    '/dist/',
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 50,
+      functions: 50,
+      lines: 50,
+      statements: 50,
+    },
+  },
+  coverageReporters: ['text', 'text-summary', 'html', 'lcov'],
+};
